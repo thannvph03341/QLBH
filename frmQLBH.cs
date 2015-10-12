b87bb7200b6a969b1ffb65b2649c99a041d515dd
thannvph03341 <thannvph03341@gmail.com>
2015-10-12 19:08:13 +0700

    asd
    asdas

---
+++ frmQLBH.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLBH
+{
+    public partial class frmQLBH : Form
+    {
+        public frmQLBH()
+        {
+            InitializeComponent();
+        }
+    }
+}
